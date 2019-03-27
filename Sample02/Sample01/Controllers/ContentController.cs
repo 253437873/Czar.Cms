2a5cce18cdfa44c0de6a37f0a948f8b50b9e2a36
253437873 <253437873@qq.com>
2019-03-27 10:55:17 +0800

    新增第三篇文章的实例代码

---
+++ Sample02/Sample01/Controllers/ContentController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample01.Models;
+
+namespace Sample01.Controllers
+{
+    public class ContentController : Controller
+    {
+
+        private readonly Content contents;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="option">这里创建IOptions<Content> option 这个对象的东西就是容器.</param>
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+
+        public IActionResult Index()
+        {
+            //var contents = new List<Content>();
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+            //}
+            //return View(new ContentViewModel { Contents = contents });
+
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+    }
+}
---
+++ Sample02/Sample01/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sample01.Models;
+
+namespace Sample01
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            //下段代码就是从appsettings.json这个配置文件中的Content这个节点匹配到Content这个对象上。
+            //services.Configure<Content>(Configuration.GetSection("Content"));//注册TOption实例对象
+
+            //下段代码就是从Content.json这个配置文件中的ContentList这个节点匹配到Content这个对象上。
+            services.Configure<Content>(Configuration.GetSection("ContentList"));//注册TOption实例对象
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
